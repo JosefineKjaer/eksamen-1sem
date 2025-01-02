00089fa664fa7e3c7e348b12d464525322de3b39
Josefine Kjaer <jokj0003@stud.kea.dk>
2025-01-02 13:24:22 +0100

    slet

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// document.querySelector(".image-container").addEventListener("mouseover", Overlay);
-
-// function Overlay() {
-//   document.querySelector(".image-container").classList.add("overlay");
-// }
