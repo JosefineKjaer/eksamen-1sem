fae8df075370c9f0447ee239baaae5deda4501d3
Josefine Kjaer <jokj0003@stud.kea.dk>
2025-01-02 13:26:28 +0100

    nye filer

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// document.querySelector(".image-container").addEventListener("mouseover", Overlay);
+
+// function Overlay() {
+//   document.querySelector(".image-container").classList.add("overlay");
+// }
